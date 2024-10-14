124536b35cc62861c073ae5485ed32c804e49475
Joshua Muster <joshuamuster@gmail.com>
2024-10-14 00:11:43 -0700

    panic commit after adding thumbnails for PDFs

--- src/Lessons/Unit01/U01L01.js
+++ src/Lessons/Unit01/U01L01.js
@@ -2,12 +2,20 @@
 import React from 'react';
 import LessonTemplate from '../../components/LessonTemplate';
 import '../../index.css';
+import ThumbnailActivity from '../../assets/img/Thumbnail-ActivityGuide-min.jpg';
+import ThumbnailSlideshow from '../../assets/img/Thumbnail-Slideshow-min.jpg';
 
 const U01L01content = () => {
   const title = "Unit 01 - Lesson 01";
-  const content = "Links to Files and Resources for Unit 01 Lesson 01 will go here!!!";
-
-  return <LessonTemplate title={title} content={content} />;
+  const content = (
+    <div>
+      <p>Links to Files and Resources for Unit 01 Lesson 01 will go here!!!</p>
+      <img className="lessonThumbnail" src={ThumbnailActivity} alt="Lesson Thumbnail"/>
+      <img className="lessonThumbnail" src={ThumbnailSlideshow} alt="Lesson Thumbnail"/>
+    </div>
+  );
+  
+  return <LessonTemplate title={title} content={content}/>;
 };
 
 export default U01L01content;
