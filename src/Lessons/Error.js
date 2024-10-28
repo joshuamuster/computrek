bdcd4ac54591458b5be91ca2a2d84e693ac859b4
Joshua Muster <joshuamuster@gmail.com>
2024-10-27 21:41:07 -0700

    add basic error page

---
+++ src/Lessons/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/lcars-ultra-classic.css';
+import '../assets/lcars-colors.css';
+import '../assets/lcars.js';
+
+const ErrorMessage = () => {
+  return (
+    <div>
+      <h1>Error: Page not found</h1>
+    </div>
+  );
+};
+
+export default ErrorMessage;
